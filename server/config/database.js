a0c20187592dd27b058bd1da8e817dd773131734
vivek-panchal <vivek.nsut@gmail.com>
2024-05-19 21:34:53 +0530

    Fic prod error

--- server/config/database.js
+++ server/config/database.js
@@ -3,8 +3,6 @@ require('dotenv').config();
 
 const databaseConnection = () => { 
     mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
     writeConcern: { w: 'majority' } 
     })
     .then(() => {
